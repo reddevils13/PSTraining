fbab57a42013364bbc18a144adc5f5c38adef274
aritra <aritramustafi56@gmail.com>
2021-06-21 16:46:10 +0530

    changes done

--- java-project/src/main/java/com/sapient/App.java
+++ java-project/src/main/java/com/sapient/App.java
@@ -6,11 +6,14 @@ package com.sapient;
  */
 public class App 
 {
-	public static void main( String[] args )
-    	{
-        	System.out.println( "Hello World!" );
-    	}
-	public static int add(int a, int b){
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to Maven Project" );
+	System.out.println( new App().add(2,3) );
+    }
+
+	public int add(int a, int b)
+	{
 		return a+b;
 	}
 }
